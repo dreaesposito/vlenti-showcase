34970616bdf6c92524dad9cae1eaae56f4699450
dreaesposito <dreaesposito@hotmail.com>
2024-09-23 22:01:54 -0500

    attempt to fix supabase error

--- src/supabseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseURL = 'https://sjzxlmlfgbpwhwobsiga.supabase.co';
-
-export const supabase = createClient(supabaseURL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
